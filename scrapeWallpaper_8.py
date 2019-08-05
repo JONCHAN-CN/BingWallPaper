3ffbad492c3054b354a6d9575f399ca837872706
JONCHAN-CN <35148796+jonchan-cn@users.noreply.github.com>
2019-08-05 11:31:49 +0800

    v1.0 released

--- scrapeWallpaper_8.py
+++ scrapeWallpaper_8.py
@@ -2,10 +2,11 @@ import requests
 import shutil
 import random
 
-dir = 'C:/Users/JON7390/OneDrive/图片/Wallpaper/'
-url = 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US'
-max_try = 10
 
+dir = 'C:/Users/JON7390/OneDrive/图片/Wallpaper/'
+url = 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=%d&n=1&mkt=en-US'
+wallpaper_num = int(input('wallper number to download(max 8):'))
+max_try = 10
 
 def randHeader():
     '''随机生成User-Agent'''
@@ -41,24 +42,27 @@ def randHeader():
     }
     return result
 
-tries =0
-while True:
-    tries += 1
-    if tries < max_try:
-        try:
-            gallery = requests.get(url, headers=randHeader(), timeout=5)
-            img_url = 'http://bing.com' + gallery.json()['images'][0]['url']
-            img_stream = requests.get(img_url, stream=True)
+if wallpaper_num > 8:
+    wallpaper_num = 8
 
-            if img_stream.status_code == 200:
-                file_name = dir + img_url.split('OHR.')[1].split('_')[0] + '.jpg'
-                with open(file_name, 'wb') as fw:
-                    shutil.copyfileobj(img_stream.raw, fw)
-                print('done getting wall paper today - %d times tried' % tries)
-                break
-        except:
-            print('trying to get wall paper today - %d times tried' % tries)
-            pass
-    else:
-        print('trying to get wall paper today - max tried exceed %d' % max_try)
-        break
+for i in range(wallpaper_num):
+    tries = 0
+    while True:
+        tries += 1
+        if tries < max_try:
+            try:
+                gallery = requests.get(url % i,headers=randHeader(), timeout=5)
+                img_url = 'http://bing.com' + gallery.json()['images'][0]['url']
+                img_stream = requests.get(img_url, stream=True)
+
+                if img_stream.status_code == 200:
+                    file_name = dir + img_url.split('OHR.')[1].split('_')[0] + '.jpg'
+                    with open(file_name, 'wb') as fw:
+                        shutil.copyfileobj(img_stream.raw, fw)
+                    print('done getting wall paper %d days ago - %d times tried' % (i, tries))
+                    break
+            except:
+                print('trying to get wall paper %d days ago - %d times tried' % (i, tries))
+        else:
+            print('trying to get wall paper today - max tried exceed %d' % max_try)
+            break
